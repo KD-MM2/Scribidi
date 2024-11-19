3ecc443e86ec2b22169dad34816bc5991b57c1e1
KaoTD <kaotd.z3@gmail.com>
2024-11-20 00:29:59 +0900

    PostgreSQL

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,26 @@
 // src/index.ts
 import express, { Express, Request, Response } from "express";
+
 // import dotenv from "dotenv";
+import prisma from "./db";
 
 // dotenv.config();
 
 const app: Express = express();
-const env = 'development';
+const env = "development";
 const port = 3000;
-const domain = env === 'development' ? 'localhost' : '';
+const domain = env === "development" ? "localhost" : "";
 
-
-app.get("/", (req: Request, res: Response) => {
-    res.send("Express + TypeScript Server");
+app.get("/", async (req: Request, res: Response) => {
+	const allUsers = await main();
+	res.send(allUsers);
 });
 
 app.listen(port, `${domain}`, () => {
-    console.log(`[server]: Server is running at http://${domain}:${port}`);
+	console.log(`[server]: Server is running at http://${domain}:${port}`);
 });
+
+async function main() {
+	const allUsers = await prisma.user.findMany();
+	return allUsers;
+}
